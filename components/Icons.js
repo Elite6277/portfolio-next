580232d00432730dd11e424932491e1d38466a8e
Elite6277 <hacklinux93@gmail.com>
2023-12-12 14:13:02 +0500

    portfolio-next_V-beta1.0

---
+++ components/Icons.js
@@ -0,0 +1,18 @@
+import {FaHtml5, FaCss3, FaJs, FaReact, FaWordpress} from "react-icons/fa";
+
+import {SiNextdotjs, SiFramer} from "react-icons/si";
+const Icons = () => {
+  return (
+    <div className="flex gap-x-4">
+      <FaHtml5 />
+      <FaCss3 />
+      <FaJs />
+      <FaReact />
+      <SiNextdotjs />
+      <SiFramer />
+      <FaWordpress />
+    </div>
+  );
+};
+
+export default Icons;
