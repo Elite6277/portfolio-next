6c9a2b8cb04d94e262d951662dba53f23359a4cb
Elite6277 <elite6277@gmail.com>
2024-02-11 14:11:58 +0500

    bug-fixes7

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react"
 import { motion } from "framer-motion"
 import { fadeIn } from "../../variants"
-// import Circles from "../../components/Circles"
+// import Circles from "../../components/Circles";
 import { BsArrowRight } from "react-icons/bs"
 import emailjs from "@emailjs/browser"
 
@@ -40,7 +40,7 @@ const Contact = () => {
             exit="hidden"
             className="h2 text-center mb-12"
           >
-            Let's <span className="text-accent">connect.</span>
+            Let&apos;s <span className="text-accent">connect.</span>
           </motion.h2>
           <motion.form
             ref={form}
@@ -81,7 +81,7 @@ const Contact = () => {
               className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
             >
               <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500 group-hover:text-[#F13024]">
-                Let's talk
+                Let&apos;s talk
               </span>
               <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px] fill-[#F13024]" />
             </button>
