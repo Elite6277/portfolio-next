f16f5985cb289c688a18b7aab09f2e403cce7441
Elite6277 <elite6277@gmail.com>
2024-02-11 14:00:48 +0500

    bug-fixes6

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,16 +1,38 @@
-import {motion} from "framer-motion";
-import {fadeIn} from "../../variants";
-
-import Circles from "/components/Circles";
-import {BsArrowRight} from "react-icons/bs";
+import React, { useRef } from "react"
+import { motion } from "framer-motion"
+import { fadeIn } from "../../variants"
+// import Circles from "../../components/Circles"
+import { BsArrowRight } from "react-icons/bs"
+import emailjs from "@emailjs/browser"
 
 const Contact = () => {
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        "service_453danu",
+        "template_an9gjwe",
+        form.current,
+        "j_i-ditFXQ4Nn3suj"
+      )
+      .then(
+        (result) => {
+          alert("SUCCESS!", result.text)
+          form.current.reset()
+        },
+        (error) => {
+          alert("FAILED...", error.text)
+        }
+      )
+  }
+
   return (
     <div className="h-full bg-primary/30">
       <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
-        {/* block */}
-        <div className="flex flex-col w-full mxa-w-[700px] ">
-          {/* text */}
+        <div className="flex flex-col w-full max-w-[700px]">
           <motion.h2
             variants={fadeIn("up", 0.2)}
             initial="hidden"
@@ -18,9 +40,11 @@ const Contact = () => {
             exit="hidden"
             className="h2 text-center mb-12"
           >
-            Let&apos;s <span className="text-accent">connect.</span>
+            Let's <span className="text-accent">connect.</span>
           </motion.h2>
           <motion.form
+            ref={form}
+            onSubmit={sendEmail}
             variants={fadeIn("up", 0.4)}
             initial="hidden"
             animate="show"
@@ -28,14 +52,36 @@ const Contact = () => {
             className="flex-1 flex flex-col gap-6 w-full mx-auto"
           >
             <div className="flex gap-x-6 w-full">
-              <input type="text" placeholder="name" className="input" />
-              <input type="text" placeholder="email" className="input" />
+              <input
+                type="text"
+                placeholder="Name"
+                name="user_name"
+                className="input"
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                name="user_email"
+                className="input"
+              />
             </div>
-            <input type="text" placeholder="subject" className="input" />
-            <textarea placeholder="message" className="textarea"></textarea>
-            <button className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
+            <input
+              type="text"
+              placeholder="Subject"
+              name="subject"
+              className="input"
+            />
+            <textarea
+              placeholder="Message"
+              name="message"
+              className="textarea"
+            ></textarea>
+            <button
+              type="submit"
+              className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
+            >
               <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500 group-hover:text-[#F13024]">
-                Let&apos;s talk
+                Let's talk
               </span>
               <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px] fill-[#F13024]" />
             </button>
@@ -43,7 +89,7 @@ const Contact = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Contact;
+export default Contact
